3a33b4971b44ebab10a25e02283d1566eb52400f
Sagar Goswami <sagargoswami2762001@gmail.com>
2022-09-15 12:28:02 +0530

    Code

---
+++ code.js
@@ -0,0 +1,6 @@
+const html = document.querySelector('html')
+const check = document.querySelector('#checkbox')
+
+check.addEventListener('change', function() {
+    html.classList.toggle('dark')
+})
